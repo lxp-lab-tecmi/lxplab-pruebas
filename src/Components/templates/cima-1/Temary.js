ce0ee53f9fb575472399a2f27c7669f987b92667
i343SPARK <pablosalas_@outlook.com>
2021-09-29 18:26:06 -0500

    LxpLab-adding-assets

--- src/Components/templates/cima-1/Temary.js
+++ src/Components/templates/cima-1/Temary.js
@@ -3,34 +3,54 @@ import { TemaryCard } from './TemaryCard'
 
 import image from '../../../assets/Tema1imgx3.png'
 import image2 from '../../../assets/Tema2imgx3.png'
+import book from '../../../assets/Book.svg'
+import video from '../../../assets/Video.svg'
+import microphone from '../../../assets/microphone.svg'
+import checklist from '../../../assets/Circul-check.svg'
 
 const dataTemaries = [{
     noTopic: 1,
     title: "Tema 1",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "00:00",
-    image: image
+    image: image,
+    book: book,
+    video: video,
+    microphone: microphone,
+    checklist: checklist
 },
 {
     noTopic: 2,
     title: "Tema 2",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "01:00",
-    image: image2
+    image: image2,
+    book: book,
+    video: video,
+    microphone: microphone,
+    checklist: checklist
 },
 {
     noTopic: 3,
     title: "Tema 3",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "03:00",
-    image: image2
+    image: image2,
+    book: book,
+    video: video,
+    microphone: microphone,
+    checklist: checklist
 },
 {
     noTopic: 4,
     title: "Tema 4",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "04:00",
-    image: image
+    image: image,
+    book: book,
+    video: video,
+    microphone: microphone,
+    checklist: checklist
 }]
 
 export const Temary = () => {
--- src/Components/templates/cima-1/TemaryCard.js
+++ src/Components/templates/cima-1/TemaryCard.js
@@ -5,20 +5,20 @@ import React from 'react'
 export const TemaryCard = ({topic}) => {
     return (
         <article className="main-temario--container">
-            <section class="main-temario--container-top">
+            <section className="main-temario--container-top">
                 <img src={topic.image} alt="" />
                 <div>
-                <h3 class ="tema-name">{topic.title}</h3>
-                <p class ="tema-description">{topic.description} </p>
+                <h3 className ="tema-name">{topic.title}</h3>
+                <p className ="tema-description">{topic.description} </p>
                 </div>
             </section>
-            <section class="main-temario--container-bottom">
-                    <p class="time">{topic.schedule} hrs</p>
+            <section className="main-temario--container-bottom">
+                    <p className="time">{topic.schedule} hrs</p>
                     <div>
-                        <span class="book"></span>
-                        <span class="video"></span>
-                        <span class="microphone"></span>
-                        <span class="checklist"></span>
+                        <span className="book"></span>
+                        <span className="video"></span>
+                        <span className="microphone"></span>
+                        <span className="checklist"></span>
                     </div>
                     {/*<a href=""><span class="start-topic"></span></a>*/}
                 </section>
