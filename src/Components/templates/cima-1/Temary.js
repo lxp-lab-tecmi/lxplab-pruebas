d367e1a6fdc3452cee7e6ea4e163b6ca1ca427b2
i343SPARK <pablosalas_@outlook.com>
2021-09-29 18:34:08 -0500

    LxpLab-adding-assets-debug

--- src/Components/templates/cima-1/Temary.js
+++ src/Components/templates/cima-1/Temary.js
@@ -3,54 +3,37 @@ import { TemaryCard } from './TemaryCard'
 
 import image from '../../../assets/Tema1imgx3.png'
 import image2 from '../../../assets/Tema2imgx3.png'
-import book from '../../../assets/Book.svg'
-import video from '../../../assets/Video.svg'
-import microphone from '../../../assets/microphone.svg'
-import checklist from '../../../assets/Circul-check.svg'
 
 const dataTemaries = [{
     noTopic: 1,
     title: "Tema 1",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "00:00",
-    image: image,
-    book: book,
-    video: video,
-    microphone: microphone,
-    checklist: checklist
+    image: image
+
 },
 {
     noTopic: 2,
     title: "Tema 2",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "01:00",
-    image: image2,
-    book: book,
-    video: video,
-    microphone: microphone,
-    checklist: checklist
+    image: image2
+
 },
 {
     noTopic: 3,
     title: "Tema 3",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "03:00",
-    image: image2,
-    book: book,
-    video: video,
-    microphone: microphone,
-    checklist: checklist
+    image: image2
 },
 {
     noTopic: 4,
     title: "Tema 4",
     description: "Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit ",
     schedule: "04:00",
-    image: image,
-    book: book,
-    video: video,
-    microphone: microphone,
-    checklist: checklist
+    image: image
+
 }]
 
 export const Temary = () => {
