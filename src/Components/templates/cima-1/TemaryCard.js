713be0952d6a31edbbfda30f09b38182e2c91141
i343SPARK <pablosalas_@outlook.com>
2021-09-29 18:54:15 -0500

    LxpLab-adding-nextTopic-Temary

--- src/Components/templates/cima-1/TemaryCard.js
+++ src/Components/templates/cima-1/TemaryCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 
@@ -20,7 +21,7 @@ export const TemaryCard = ({topic}) => {
                         <span className="microphone"></span>
                         <span className="checklist"></span>
                     </div>
-                    {/*<a href=""><span class="start-topic"></span></a>*/}
+                    <Link className="circle-link" href=""><span className="start-topic"></span></Link>
                 </section>
         </article>
     )
