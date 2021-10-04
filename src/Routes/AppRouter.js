28c359478a8d3282f585f901ee73525f3efeed72
Cristian Josafat Flores Ramírez <josafat.flores@tecmilenio.mx>
2021-10-04 14:55:17 -0500

    add filter screen

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import { CatalogueScreen } from "../Components/Catalogue/CatalogueScreen";
 import { IndexScreen } from "../Components/Index/IndexScreen";
 import { VideoQuestionary } from "../Components/VideoWithQuestions/VideoWithQuestions";
 
@@ -8,6 +9,7 @@ export default function AppRouter() {
     <Switch>
       <Route exact path="/" component={IndexScreen} />
       <Route exact path="/videowithquestions" component={VideoQuestionary} />
+      <Route exact path="/filter" component={CatalogueScreen} />
       <Route render={() => <IndexScreen />} />
     </Switch>
   );
