f6450402ffe243a91d7e4e82c35da4d876523385
i343SPARK <pablosalas_@outlook.com>
2021-10-06 14:24:23 -0500

    LxpLab-topic1-screen

--- src/Components/templates/cima1-topic1/Topic1Screen.js
+++ src/Components/templates/cima1-topic1/Topic1Screen.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { NavBarCima1 } from '../cima-1/NavBarCima1'
+import { BackButton } from './BackButton'
 
 export const Topic1Screen = () => {
     return (
         <div>
-            
+            <NavBarCima1 />
+            <BackButton />
         </div>
     )
 }
