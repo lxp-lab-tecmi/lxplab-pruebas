cd4d703ea0f53fb61d60eefae5307255201c9953
i343SPARK <pablosalas_@outlook.com>
2021-10-08 13:31:59 -0500

    LxpLab-Adding-Top

--- src/Components/templates/cima1-topic1/TitleTopic1.js
+++ src/Components/templates/cima1-topic1/TitleTopic1.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export const TitleTopic1 = () => {
     return (
-        <>
-            <h2>Tema 1.</h2>
-        </>
+        <section className='topic1-title'>
+            <h2 className='h2t1'>Tema 1.</h2>
+            <h1 className='h1t1'>Lorem ipsum </h1>
+            <p className='pt1'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismodtin.</p>
+            <div className='title-t1-bottom'>
+                <span className='t1-heart'></span>
+                <p className='t1-time'>00:00hrs</p>
+                <span className='t1-book'></span>
+            </div>
+        </section>
     )
 }
--- src/Components/templates/cima1-topic1/Topic1Screen.js
+++ src/Components/templates/cima1-topic1/Topic1Screen.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { NavBarCima1 } from '../cima-1/NavBarCima1'
 import { BackButton } from './BackButton'
+import { TitleTopic1 } from './TitleTopic1'
 
 export const Topic1Screen = () => {
     return (
         <div>
             <NavBarCima1 />
             <BackButton />
+            <TitleTopic1 />
         </div>
     )
 }
