e2e617c4ba1ba83e64457d3d3b8aaaae81da8405
i343SPARK <pablosalas_@outlook.com>
2021-10-15 17:05:53 -0500

    lxp-testeo

--- src/Components/templates/cima1-topic1/ImageTopic1.js
+++ src/Components/templates/cima1-topic1/ImageTopic1.js
@@ -4,7 +4,7 @@ import image from '../../../assets/Group2x2.jpg'
 export const ImageTopic1 = () => {
     return (
         <div className='t1-limit-container'>
-            <img className='topic1-image' src={image} />
+            <img className='topic1-image' src={image} alt='portada'/>
         </div>
     )
 }
