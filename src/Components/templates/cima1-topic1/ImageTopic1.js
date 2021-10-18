710e55ee97c0bcbd896935ddd55458f1289179c6
i343SPARK <pablosalas_@outlook.com>
2021-10-18 02:20:34 -0500

    lxplabs-justupdates

--- src/Components/templates/cima1-topic1/ImageTopic1.js
+++ src/Components/templates/cima1-topic1/ImageTopic1.js
@@ -3,8 +3,6 @@ import image from '../../../assets/Group2x2.jpg'
 
 export const ImageTopic1 = () => {
     return (
-        <div className='t1-limit-container'>
-            <img className='topic1-image' src={image} alt='portada'/>
-        </div>
+        <div className='t1-limit-container'></div>
     )
 }
