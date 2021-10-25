a346f05fa163cc00b546f7f5155963cfc6066167
Cristian Josafat Flores Ramírez <josafat.flores@tecmilenio.mx>
2021-10-25 12:45:42 -0500

    useFetch file added

---
+++ src/Hooks/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef } from 'react'
+const axios = require('axios');
+
+export const baseUrl = "https://apazrtes-exp06-01.azure-api.net"
+
+export const useFetch = (url) => {
+
+    const isMounted = useRef(true)
+    const [state, setState] = useState({ data: null, loading: true, error: null })
+
+    useEffect(() => {
+        return () => {
+            isMounted.current = false
+        }
+    }, [])
+
+    useEffect(() => {
+        setState({ data: null, loading: true, error: null })
+        axios.get(url)
+            .then(data => {
+                if (isMounted.current) {
+                    if(data.data.code === 1){
+                        setState({
+                            data: data.data.data,
+                            loading: false,
+                            error: null
+                        })
+                    }
+                }
+            })
+            .catch(error => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: error.message
+                })
+            })
+
+    }, [url])
+
+    return state;
+}
